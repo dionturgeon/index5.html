3d8e4240c25e8f5c30743ec1055eeec292c64fa9
Sean Turgeon <dionturgeon@yahoo.com>
2026-01-10 10:58:06 -0800

    first commit

---
+++ primenumbers.py
@@ -0,0 +1,16 @@
+def check_prime(n):
+    if n <= 1:
+        print(f"{n} is not a prime number.")
+        return
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            print(f"{n} is not a prime number.")
+            return
+
+    print(f"{n} is a prime number.")
+
+
+# Example usage
+check_prime(7)
+check_prime(10)

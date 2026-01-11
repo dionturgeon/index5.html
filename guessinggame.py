592b50f561d138dabc4d32c30df148540cf60046
Sean Turgeon <dionturgeon@yahoo.com>
2026-01-10 19:48:17 -0800

    practicing guessing game

---
+++ guessinggame.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1, 100)
+guess = None
+
+print("I'm thinking of a number between 1 and 100.")
+
+while guess != number:
+    guess = int(input("Enter your guess: "))
+
+    if guess < number:
+        print("Too low! Try again.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You guessed the number.")
